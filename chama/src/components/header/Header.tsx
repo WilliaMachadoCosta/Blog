0ee3811c43c0ea92620719502ac9752022da0859
williammachadocosta <williammachadocosta.trabalho@gmail.com>
2025-06-16 20:10:32 -0300

    feat: adicionando componente header

---
+++ chama/src/components/header/Header.tsx
@@ -0,0 +1,6 @@
+export function Header() {
+    <div className="flex items-center bg-amber-300">
+        <div className="border-4 border-b-black border-t-transparent rounded-b-full">
+        </div>
+    </div>
+}

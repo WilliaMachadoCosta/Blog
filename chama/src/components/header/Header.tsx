ff6d3b70beec3ec40fd60318e8f482c5b519682f
williammachadocosta <williammachadocosta.trabalho@gmail.com>
2025-06-16 21:37:10 -0300

    feat:adicionado header inicial semelhante ao whatsapp

--- chama/src/components/header/Header.tsx
+++ chama/src/components/header/Header.tsx
@@ -1,9 +1,26 @@
-export function Header() {
-    return (
-        <div className="flex items-center bg-amber-300 h-80">
-            <div className="border-4 border-b-black border-t-transparent rounded-b-full h-40 w-40">
-            </div>
-        </div>
-    )
 
+'use client'
+import { Search, MessageCircle, MoreVertical, FireExtinguisher } from 'lucide-react';
+
+export default function Header() {
+    return (
+        <header className="flex items-center justify-between bg-[#008069] px-4 py-2 shadow-md">
+            <div className="flex items-center gap-2 text-white text-xl font-semibold">
+                Chama <span className="text-orange-500">🔥</span> No ZAP
+            </div>
+
+            {/* Ações */}
+            <div className="flex items-center gap-4 text-white">
+                <button className="hover:text-gray-300">
+                    <Search size={20} />
+                </button>
+                <button className="hover:text-gray-300">
+                    <MessageCircle size={20} />
+                </button>
+                <button className="hover:text-gray-300">
+                    <MoreVertical size={20} />
+                </button>
+            </div>
+        </header>
+    );
 }

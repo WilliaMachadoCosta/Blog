3204841f589839dff4991857a1d2605ee6fa709a
williammachadocosta <williammachadocosta.trabalho@gmail.com>
2025-06-22 13:54:26 -0300

    mudar a cor do spin

--- chama/src/components/SpinLoad/SpinLoader.tsx
+++ chama/src/components/SpinLoad/SpinLoader.tsx
@@ -1,7 +1,7 @@
 export function SpinLoader() {
     return (
         <div className="flex justify-center items-center h-screen bg-[#202C33]">
-            <div className="w-12 h-12 border-4 border-t-transparent border-green-500 rounded-full animate-spin"></div>
+            <div className="w-12 h-12 border-4 border-t-transparent border-green-950 rounded-full animate-spin"></div>
         </div>
     );
 }

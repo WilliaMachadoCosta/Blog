208392fe581db29326e237e01a89982fe3d452a8
williammachadocosta <williammachadocosta.trabalho@gmail.com>
2025-07-05 22:50:45 -0300

    wip

--- chama/src/components/banner/google-ads.tsx
+++ chama/src/components/banner/google-ads.tsx
@@ -70,7 +70,7 @@ export default function GoogleAd({ className = '' }: GoogleAdProps) {
                     // style={{ display: 'block' }}
                     // data-ad-client="ca-pub-5074393689985715"
                     // data-ad-slot="9365926617"
-                    className="adsbygoogle adslot_1 grande"
+                    className="adsbygoogle adslot_1"
                     style={{
                         display: 'block',
                         width: '100%',

97b9cd136a37ec5c19ed0912aaa4598690e52621
williammachadocosta <williammachadocosta.trabalho@gmail.com>
2025-07-06 11:53:36 -0300

    fix corrigindo layout ads

--- chama/src/components/banner/google-ads.tsx
+++ chama/src/components/banner/google-ads.tsx
@@ -42,7 +42,7 @@ export default function GoogleAd({ className = '' }: GoogleAdProps) {
                     .adslot_1 {
                         display: inline;
                         width: 320px;
-                        height: 250px;
+                        height: 100px;
                     }
                     @media (min-width: 500px) {
                         .adslot_1 {
@@ -67,7 +67,7 @@ export default function GoogleAd({ className = '' }: GoogleAdProps) {
             <div ref={adRef} className={`w-full flex justify-center ${className}`}>
                 <ins
                     className="adsbygoogle adslot_1"
-                    style={{ display: 'inline', width: '320px', height: '250px' }}
+                    style={{ display: 'inline', width: '320px', height: '100px' }}
                     data-ad-client="ca-pub-5074393689985715"
                     data-ad-slot="9365926617"
 

ecd7abe3276eedbd8c2c76dd49dbcf663130c7f5
williammachadocosta <williammachadocosta.trabalho@gmail.com>
2025-07-08 09:57:59 -0300

    sitemap com 10 posts

--- chama/src/app/sitemap.ts
+++ chama/src/app/sitemap.ts
@@ -1,10 +1,16 @@
+import { getAllPosts } from "@/services/postServices";
 import { MetadataRoute } from "next";
+
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrl = 'https://chamanozap.net';
   const currentDate = new Date();
 
-  return [
+  // Buscar posts do seu serviço
+  const posts = await getAllPosts();
+
+  // URLs fixas
+  const staticRoutes: MetadataRoute.Sitemap = [
     {
       url: baseUrl,
       lastModified: currentDate,
@@ -30,4 +36,14 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       priority: 0.7,
     },
   ];
+
+  // URLs de posts
+  const postRoutes: MetadataRoute.Sitemap = posts.map(post => ({
+    url: `${baseUrl}/blog/${post.slug}`,
+    lastModified: new Date(post.modified || post.date),
+    changeFrequency: 'weekly',
+    priority: 0.7,
+  }));
+
+  return [...staticRoutes, ...postRoutes];
 }

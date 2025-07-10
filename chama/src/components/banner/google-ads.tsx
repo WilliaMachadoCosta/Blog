3b0aade13ea5503af4f1383208fc5cbbea1fe628
williammachadocosta <williammachadocosta.trabalho@gmail.com>
2025-07-10 12:40:50 -0300

    wip

--- chama/src/components/banner/google-ads.tsx
+++ chama/src/components/banner/google-ads.tsx
@@ -67,7 +67,7 @@ export default function GoogleAd({ className = '' }: GoogleAdProps) {
             <div ref={adRef} className={`w-full flex justify-center ${className}`}>
                 <ins
                     className="adsbygoogle"
-                    style={{ display: 'block', width: '320px', height: '100px' }}
+                    style={{ display: 'block' }}
                     data-ad-client="ca-pub-5074393689985715"
                     data-ad-slot="9365926617"
                     data-ad-format="auto"

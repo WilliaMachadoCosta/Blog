e71f3a0872d2c81c90ad4035f4ebe720248cbfb4
williammachadocosta <williammachadocosta.trabalho@gmail.com>
2025-07-15 20:58:23 -0300

    add clarity

--- chama/src/app/layout.tsx
+++ chama/src/app/layout.tsx
@@ -43,6 +43,20 @@ export default function RootLayout({
           crossOrigin="anonymous"
         />
 
+        {/* Microsoft Clarity Script */}
+        <Script
+          id="clarity-script"
+          strategy="afterInteractive"
+        >
+          {`
+      (function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "nviib1pqrd");
+    `}
+        </Script>
+
       </head>
 
       {/* Google Analytics */}

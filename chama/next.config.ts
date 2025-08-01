5778425bdab5192a44c3731c1b9b081c47b6e2ab
williammachadocosta <williammachadocosta.trabalho@gmail.com>
2025-08-01 07:22:00 -0300

    desabilitando otimizacao imagens vercel

--- chama/next.config.ts
+++ chama/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true, // Desativa a otimização de imagens da Vercel
     domains: ['api.chamanozap.net', 'res.cloudinary.com'],
   },
 
